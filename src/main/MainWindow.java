b7b7bacd0c2e1d2daba1b5a8ca15d3f60c2a3953
icedany <90800013+icedany36@users.noreply.github.com>
2022-05-19 15:53:44 +0200

    cose cosose

--- src/main/MainWindow.java
+++ src/main/MainWindow.java
@@ -2,8 +2,11 @@ package main;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -12,19 +15,26 @@ public class MainWindow extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("MainWindowInterface.fxml"));
-        Scene scene = new Scene(loader.load());
-        scene.setFill(Color.TRANSPARENT);
-        stage.setScene(scene);
+
+        Parent root = FXMLLoader.load(getClass().getResource("../main/MainWindowInterface.fxml"));
+
+        stage.setScene(new Scene(root));
+        // scene.setFill(Color.TRANSPARENT);
         stage.initStyle(StageStyle.TRANSPARENT);
         stage.setResizable(false);
         stage.setTitle("Calculator");
         stage.getIcons().add(new Image(getClass().getResourceAsStream("/icon.png")));
-        ((MainWindowController)loader.getController()).init(stage);
+        // ((MainWindowController)loader.getController()).init(stage);
         stage.show();
+
+
     }
 
-    public void run() {
-        launch();
+    public static void main(String[] args) {
+
+        launch(args);
+    }
+
+    public static void test() {
     }
 }
